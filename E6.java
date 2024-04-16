7c81444b36e44bc85e2edfcf92607d5582467b79
ch052877 <christyanmend@gmail.com>
2024-04-16 16:29:22 -0300

    E6 E7

--- E6.java
+++ E6.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class E6 {
     public static void main(String[] args) {
-        System.out.println("blabla");
+        Scanner scan = new Scanner(System.in);
+        double pi = 3.14;
+        System.out.print("Digite o valor do raio a ser calculado a área : ");
+        double value = scan.nextInt();
+        double resultado = (value * value * pi);
+        scan.close();
+        System.out.println("\nO valor da área é " + resultado );
     }
     
 }
