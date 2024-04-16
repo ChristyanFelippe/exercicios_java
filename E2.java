92e022b521f629a97dc18b7251e0692a9b9af09d
ch052877 <christyanmend@gmail.com>
2024-04-15 23:26:22 -0300

    E2

--- E2.java
+++ E2.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class E2 {
     public static void main(String[] args) {
-        
+        System.out.print("Digite um numero : ");
+        Scanner scanner = new Scanner(System.in);
+        int numeroDigitado = scanner.nextInt();
+        System.out.println("Numero digitado foi : " + numeroDigitado);
     }
 }
